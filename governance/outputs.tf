01b65e5d034343de9a28d016b51ad13751f711bb
Egor Shulga <egorshulga@outlook.com>
2021-12-13 18:19:30 +0300

    move compartment setup to module

---
+++ governance/outputs.tf
@@ -0,0 +1,3 @@
+output "compartment_id" {
+  value = oci_identity_compartment.tf_compartment.id
+}
--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
+module "governance" {
+  source       = "./governance"
+  tenancy_ocid = var.tenancy_ocid
+}
+
 module "network" {
   source         = "./network"
-  compartment_id = oci_identity_compartment.tf_compartment.id
+  compartment_id = module.governance.compartment_id
   region         = var.region
 }
 
 module "compute" {
   source         = "./compute"
-  compartment_id = oci_identity_compartment.tf_compartment.id
+  compartment_id = module.governance.compartment_id
   ssh_key_pub    = var.ssh_key_pub
   subnet_id      = module.network.subnet_id
 
