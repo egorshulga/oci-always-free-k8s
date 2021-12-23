1a6586b561182a63244f9921a96fde4bfafbfa01
Egor Shulga <egorshulga@outlook.com>
2021-12-23 22:09:20 +0300

    worker: script fix

--- compute/bootstrap/scripts/setup-worker.sh
+++ compute/bootstrap/scripts/setup-worker.sh
@@ -2,8 +2,9 @@
 
 # k8s requires 2 CPUs. We override this requirement (to better use resources from the Always Free tier).
 # We also ignore memory requirement, as for testing we deploy cluster to micro shapes.
+# Currently we don't pass control plane's CA public key.
 sudo kubeadm join \
   --ignore-preflight-errors NumCPU,Mem \
   --token=${token} \
-  --discovery-token-unsafe-skip-ca-verification \ # Currently we don't pass control plane's CA public key.
+  --discovery-token-unsafe-skip-ca-verification \
   ${leader-fqdn}:6443
