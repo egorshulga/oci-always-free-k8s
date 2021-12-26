9aafe7f67adb073392a1baba0d65d9d93498a4c4
Egor Shulga <egorshulga@outlook.com>
2021-12-26 14:11:26 +0300

    'apt-get clean' before 'apt-get update' (to overcome possible apt repos cache issues)

--- cluster-provisioning/compute/bootstrap/scripts/install-kubeadm.sh
+++ cluster-provisioning/compute/bootstrap/scripts/install-kubeadm.sh
@@ -32,6 +32,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get clean
 sudo apt-get update
 
 # 1.3. Install containerd
@@ -57,6 +58,7 @@ sudo systemctl restart containerd
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get clean
 sudo apt-get update
 sudo apt-get install --yes kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
