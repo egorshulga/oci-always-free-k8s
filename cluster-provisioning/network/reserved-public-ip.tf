139c38c07787cac614608ba9d579485335281838
Egor Shulga <egorshulga@outlook.com>
2021-12-31 17:40:03 +0300

    public-ip: fix terraform trying to remove public ip assignment from load balancer

--- cluster-provisioning/network/reserved-public-ip.tf
+++ cluster-provisioning/network/reserved-public-ip.tf
@@ -2,4 +2,7 @@ resource "oci_core_public_ip" "reserved" {
   compartment_id = var.compartment_id
   lifetime       = "RESERVED"
   display_name   = "reserved-ip"
+  lifecycle {
+    ignore_changes = [private_ip_id] # Terraform removes load balancer IP assignment on subsequent updates, so we ignore this field.
+  }
 }
