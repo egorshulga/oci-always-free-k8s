8c38fa338544dec43521f32c625da9db2d72566c
Egor Shulga <egorshulga@outlook.com>
2022-01-08 17:42:03 +0300

    k8s: apply setup scripts without pushing them to instances

--- cluster-provisioning/k8s/control-plane.tf
+++ cluster-provisioning/k8s/control-plane.tf
@@ -19,52 +19,27 @@ resource "null_resource" "control_plane_setup" {
     private_key = file(self.triggers.ssh_key_path)
     timeout     = "5m"
   }
+  provisioner "remote-exec" { inline = ["echo 'Running leader init script'"] }
+  provisioner "remote-exec" { inline = [file("${path.module}/scripts/update-upgrade.sh")] }
+  provisioner "remote-exec" { inline = [local.script.reset-iptables] }
+  provisioner "remote-exec" { inline = [local.script.install-kubeadm] }
   provisioner "remote-exec" {
-    inline     = ["mkdir init"]
-    on_failure = continue
-  }
-  provisioner "file" {
-    content     = local.script.reset-iptables
-    destination = "/home/ubuntu/init/reset-iptables.sh"
-  }
-  provisioner "file" {
-    content     = local.script.install-kubeadm
-    destination = "/home/ubuntu/init/install-kubeadm.sh"
-  }
-  provisioner "file" {
-    content     = file("${path.module}/scripts/prepare-kube-config-for-cluster.sh")
-    destination = "/home/ubuntu/init/prepare-kube-config-for-cluster.sh"
-  }
-  provisioner "file" {
-    content = templatefile("${path.module}/scripts/prepare-kube-config-for-external.sh", {
-      leader-fqdn            = var.leader.fqdn,
-      cluster-public-address = var.cluster_public_address != null ? var.cluster_public_address : var.cluster_public_ip,
-    })
-    destination = "/home/ubuntu/init/prepare-kube-config-for-external.sh"
-  }
-  provisioner "file" {
-    content = templatefile("${path.module}/scripts/setup-control-plane.sh", {
+    inline = [templatefile("${path.module}/scripts/setup-control-plane.sh", {
       k8s_discovery_token = local.k8s_discovery_token,
       node-name           = var.leader.hostname,
       leader-fqdn         = var.leader.fqdn,
       cluster-public-ip   = var.cluster_public_ip,
       cluster-dns-name    = var.cluster_public_address
-    })
-    destination = "/home/ubuntu/init/setup-control-plane.sh"
+    })]
   }
-  provisioner "file" {
-    content     = file("${path.module}/scripts/setup-cluster-network.sh")
-    destination = "/home/ubuntu/init/setup-cluster-network.sh"
+  provisioner "remote-exec" { inline = [file("${path.module}/scripts/prepare-kube-config-for-cluster.sh")] }
+  provisioner "remote-exec" {
+    inline = [templatefile("${path.module}/scripts/prepare-kube-config-for-external.sh", {
+      leader-fqdn            = var.leader.fqdn,
+      cluster-public-address = var.cluster_public_address != null ? var.cluster_public_address : var.cluster_public_ip,
+    })]
   }
-  provisioner "remote-exec" { inline = ["echo 'Running leader init script'"] }
-  provisioner "remote-exec" { inline = [file("${path.module}/scripts/update-upgrade.sh")] }
-  provisioner "remote-exec" { inline = ["chmod 0777 ~/init/*"] }
-  provisioner "remote-exec" { inline = ["~/init/reset-iptables.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/install-kubeadm.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/setup-control-plane.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/prepare-kube-config-for-cluster.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/prepare-kube-config-for-external.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/setup-cluster-network.sh"] }
+  provisioner "remote-exec" { inline = [file("${path.module}/scripts/setup-cluster-network.sh")] }
   provisioner "remote-exec" { inline = ["echo 'Leader init script complete'"] }
   provisioner "remote-exec" { inline = ["sudo bash -c \"echo 'This is a leader instance, which was provisioned by Terraform' >> /etc/motd\""] }
 
@@ -80,7 +55,7 @@ resource "null_resource" "control_plane_setup" {
     when       = destroy
     on_failure = continue
     inline = [
-      "kubectl drain ${self.triggers.hostname} --force",
+      "kubectl drain ${self.triggers.hostname} --force --ignore-daemonsets",
       "kubectl delete node ${self.triggers.hostname} --force",
       "sudo kubeadm reset --force",
     ]
--- cluster-provisioning/k8s/workers.tf
+++ cluster-provisioning/k8s/workers.tf
@@ -5,6 +5,7 @@ resource "null_resource" "worker_setup" {
     control_plane_init = null_resource.control_plane_setup.id # Workers will be init'ed after control-plane
 
     private_ip   = each.value.private_ip
+    hostname     = each.value.hostname
     vm_user      = each.value.vm_user
     bastion_host = var.cluster_public_ip
     ssh_key_path = var.ssh_key_path
@@ -20,45 +21,35 @@ resource "null_resource" "worker_setup" {
     bastion_private_key = file(self.triggers.ssh_key_path)
     timeout             = "5m"
   }
+  provisioner "remote-exec" { inline = ["echo 'Running worker init script'"] }
   provisioner "remote-exec" {
-    inline     = ["mkdir .kube init"]
+    inline     = ["mkdir .kube"]
     on_failure = continue
   }
   provisioner "file" {
     source      = ".terraform/.kube/config-cluster"
     destination = ".kube/config"
   }
-  provisioner "file" {
-    content     = local.script.reset-iptables
-    destination = "/home/ubuntu/init/reset-iptables.sh"
-  }
-  provisioner "file" {
-    content     = local.script.install-kubeadm
-    destination = "/home/ubuntu/init/install-kubeadm.sh"
-  }
-  provisioner "file" {
-    content = templatefile("${path.module}/scripts/setup-worker.sh", {
+  provisioner "remote-exec" { inline = [file("${path.module}/scripts/update-upgrade.sh")] }
+  provisioner "remote-exec" { inline = [local.script.reset-iptables] }
+  provisioner "remote-exec" { inline = [local.script.install-kubeadm] }
+  provisioner "remote-exec" {
+    inline = [templatefile("${path.module}/scripts/setup-worker.sh", {
       leader_url          = var.leader.fqdn,
       k8s_discovery_token = local.k8s_discovery_token,
       node_name           = each.value.hostname,
-    })
-    destination = "/home/ubuntu/init/setup-worker.sh"
+    })]
   }
-  provisioner "remote-exec" { inline = ["echo 'Running worker init script'"] }
-  provisioner "remote-exec" { inline = [file("${path.module}/scripts/update-upgrade.sh")] }
-  provisioner "remote-exec" { inline = ["chmod 0777 ~/init/*"] }
-  provisioner "remote-exec" { inline = ["~/init/reset-iptables.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/install-kubeadm.sh"] }
-  provisioner "remote-exec" { inline = ["~/init/setup-worker.sh"] }
   provisioner "remote-exec" { inline = ["echo 'Worker init script complete'"] }
   provisioner "remote-exec" { inline = ["sudo bash -c \"echo 'This is a worker instance, which was provisioned by Terraform' >> /etc/motd\""] }
 
   provisioner "remote-exec" {
-    when = destroy
-    inline = [
-      "kubectl drain ${self.triggers.hostname} --force",
-      "kubectl delete node ${self.triggers.hostname}",
-    ]
+    when       = destroy
     on_failure = continue
+    inline = [
+      "kubectl drain ${self.triggers.hostname} --force --ignore-daemonsets",
+      "kubectl delete node ${self.triggers.hostname}",
+      "sudo kubeadm reset --force",
+    ]
   }
 }
