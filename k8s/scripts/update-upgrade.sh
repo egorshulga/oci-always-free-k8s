b4fcda6979700aa9b1b5a4d75e7b71cf3e665082
Egor Shulga <egorshulga@outlook.com>
2022-01-13 11:07:08 +0300

    run clean before apt-get update (to elminate possible stuck caches issue)

--- k8s/scripts/update-upgrade.sh
+++ k8s/scripts/update-upgrade.sh
@@ -4,5 +4,6 @@
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 
+sudo apt-get clean
 sudo apt-get update --yes
 sudo apt-get upgrade --yes
